596e0af08c7edd36ad489e70cf3205518b932b87
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-06-09 14:12:10 +0300

    [course1] - задание 1 перенос вёрстки counter

---
+++ course/section0/demo/src/layouts/AppLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app_layout">
+    <app-layout-header/>
+    <slot/>
+  </div>
+</template>
+<script setup>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+</script>
