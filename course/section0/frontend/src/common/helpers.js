5d94ef2e5fb436a11489384dab21360187ce8409
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-06-21 12:21:49 +0300

    [1.26] - Добавил вспомогательные функции и константы

---
+++ course/section0/frontend/src/common/helpers.js
@@ -0,0 +1,47 @@
+import {
+  DAY_IN_MILLISEC,
+  TAG_SEPARATOR
+} from './constants';
+import timeStatuses from './enums/timeStatuses';
+import taskStatuses from './enums/taskStatuses';
+
+/***
+ * Разделение строки с метками по разделителю
+ * @param tags{String} - строка с метками
+ * @returns {String[]} - массив со статусами
+ */
+export const getTagsArrayFromString = tags => {
+  const array = tags.split(TAG_SEPARATOR);
+  return array.slice(1, array.length);
+};
+
+/***
+ * Проверка на дедлайн
+ * @param dueDate{String} - срок выполнения задачи
+ * @returns {String}
+ */
+export const getTimeStatus = dueDate => {
+  if (!dueDate) {
+    return '';
+  }
+  const currentTime = +new Date();
+  const taskTime = Date.parse(dueDate);
+  const timeDelta = taskTime - currentTime;
+  if (timeDelta > DAY_IN_MILLISEC) {
+    return '';
+  }
+  return timeDelta < 0 ? timeStatuses.DEADLINE : timeStatuses.EXPIRED;
+};
+
+/***
+ * Функция нормализации задач
+ * @param task{Object} - задачи
+ * @returns {*&{timeStatus: String, status: String}}
+ */
+export const normalizeTask = task => {
+  return {
+    ...task,
+    status: task.statusId ? taskStatuses[task.statusId] : '',
+    timeStatus: getTimeStatus(task.dueDate)
+  };
+};
