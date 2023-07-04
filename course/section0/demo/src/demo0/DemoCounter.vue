2c30bb8b7ff056c9643960b5f3a9341c129431a6
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-07-04 17:30:39 +0300

    [1.35] - Добавил стили для счётчика

--- course/section0/demo/src/demo0/DemoCounter.vue
+++ course/section0/demo/src/demo0/DemoCounter.vue
@@ -15,3 +15,27 @@ const decrement = () => count.value -= 1;
 const increment = () => count.value += 1;
 
 </script>
+<style scoped lang="scss">
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    width: 2rem;
+    height: 2rem;
+
+    font-size: 1.5rem;
+    color: white;
+
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    cursor: pointer;
+
+    &:hover {
+      background: #ff0000;
+    }
+  }
+}
+</style>
