cf65e1dd52ea3bf1c3cdd2aa5a844e6222a5025c
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-08-04 14:52:06 +0300

    [2.9] - Добавил блоки counter

---
+++ course/section0/demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,45 @@
+<template>
+  <DemoCounter>
+    <div class="counter">
+      <CounterButton @action="decrement"> - </CounterButton>
+      <CounterValue :value="count" />
+      <CounterButton @action="increment"> + </CounterButton>
+    </div>
+  </DemoCounter>
+</template>
+<script setup>
+import {ref} from "vue";
+import CounterButton from '@/demo1/CounterButton.vue';
+import CounterValue from '@/demo1/CounterValue.vue';
+
+const count = ref(0);
+
+const decrement = () => count.value -= 1;
+
+const increment = () => count.value += 1;
+
+</script>
+<style scoped lang="scss">
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    width: 2rem;
+    height: 2rem;
+
+    font-size: 1.5rem;
+    color: white;
+
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    cursor: pointer;
+
+    &:hover {
+      background: #ff0000;
+    }
+  }
+}
+</style>
