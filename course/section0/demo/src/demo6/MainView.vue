f0f260a9aad31ce251d7e6b45612a591c51f70e0
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-08-15 19:31:27 +0300

    [6.5] - анимация на js

--- course/section0/demo/src/demo6/MainView.vue
+++ course/section0/demo/src/demo6/MainView.vue
@@ -8,6 +8,18 @@
   <transition name="bounce">
     <img v-if="show" src="@/assets/img/admin.jpg" alt="img">
   </transition>
+  <demo-button
+    @mouseenter="show2 = !show2"
+    @mouseleave="show2 = !show2"
+  >Показать</demo-button>
+  <br>
+  <transition
+    @enter="enterAnimation"
+    @leave="leaveAnimation"
+    :css="false"
+  >
+    <img v-if="show2" src="@/assets/img/admin.jpg" alt="img">
+  </transition>
 </template>
 
 <script setup>
@@ -17,6 +29,24 @@ import CollapseButton from '@/demo6/CollapseButton.vue';
 import { ref } from 'vue'
 
 const show = ref(true)
+const show2 = ref(true)
+
+function clearAnimations (el, done) {
+  el.addEventListener('animationend', () => {
+    el.className = ''
+    done()
+  })
+}
+
+function enterAnimation (el, done) {
+  el.className = 'bounce'
+  clearAnimations(el, done)
+}
+
+function leaveAnimation (el, done) {
+  el.className = 'bounce-reverse'
+  clearAnimations(el, done)
+}
 </script>
 
 <style scoped>
@@ -27,6 +57,26 @@ const show = ref(true)
   animation: bounce-in .5s reverse;
 }
 @keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.bounce {
+  animation: bounce .5s;
+}
+
+.bounce-reverse {
+  animation: bounce .5s reverse;
+}
+
+@keyframes bounce {
   0% {
     transform: scale(0);
   }
